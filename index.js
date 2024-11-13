a060fc834090e17b476bb65a1a2620c7a2f8cd71
Edgarisidro <127244287+Edgarisidro@users.noreply.github.com>
2024-11-12 21:45:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('te amo, me gustas mucho <3')
+    alert('Feliz cumpleaños pasatela bonito<3')
+     // Crear un nuevo elemento de imagen
+    const img = document.createElement('img');
+    img.src = 'https://i.pinimg.com/736x/ff/89/0c/ff890c2cd6b6b717a25260f7a9ba29d2.jpg'; // Reemplaza con la URL o ruta de tu imagen
+    img.alt = 'Imagen de feliz cumpleaños';
+    img.style.width = '200px'; // Ajusta el tamaño según sea necesario
+    img.style.marginTop = '20px'; // Opcional: agrega un margen superior
+    
+    // Añadir la imagen al documento, por ejemplo al final del body
+    document.body.appendChild(img);
 });
 
 const noBtn = document.querySelector('#noBtn');
